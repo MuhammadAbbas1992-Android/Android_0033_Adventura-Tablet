4d8df4047fd55b8f562c1d1a4a987074d228a140
Muhammad abbas <g.abbas1992@gmail.com>
2023-12-16 20:30:05 +0500

    Created All screens for TabletApp

--- app/src/main/java/com/example/adventura/LoginActivity.java
+++ app/src/main/java/com/example/adventura/LoginActivity.java
@@ -2,13 +2,49 @@ package com.example.adventura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.adventura.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding= ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLogin.setOnClickListener(view -> {
+            showAPIDialog();
+        });
+    }
+    private void showAPIDialog()
+    {
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_api_end_point);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.show();
+
+        TextView close=dialog.findViewById(R.id.tvClose);
+        TextView done=dialog.findViewById(R.id.tvDone);
+
+        close.setOnClickListener(view -> {
+            dialog.dismiss();
+        });
+        done.setOnClickListener(view -> {
+            dialog.dismiss();
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            finish();
+        });
+
     }
 }
--- app/src/main/java/com/example/adventura/SplashActivity.java
+++ app/src/main/java/com/example/adventura/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.adventura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                // The user isn't logged in
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+
+        }, 2000);
+
     }
 }
